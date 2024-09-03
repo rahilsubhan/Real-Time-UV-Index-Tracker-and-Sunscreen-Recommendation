ea4cfbd97fc44a6dfc39a7f26a2ceea61f0ae650
mdrahilsubhan2 <mdrahilsubhan26@gmail.com>
2024-09-03 21:13:55 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 import express from "express";
 import axios from "axios";
+import https from "https";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://api.openuv.io/api/v1/uv?lat=17.50&lng=78.36";
+const numbers = new Array(2); 
 
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
+
+async function fetchDataAndProcess() {
+  const options = {
+    hostname: 'ipinfo.io',
+    path: '/json',
+  }; 
+
+  return new Promise((resolve, reject) => {
+    https.get(options, (res) => {
+      let dataChunks = [];
+
+      res.on('data', (chunk) => {
+        dataChunks.push(chunk);
+      });
+
+      res.on('end', () => {
+        const data = Buffer.concat(dataChunks).toString();
+        const locationData = JSON.parse(data);
+        const Latitude = locationData.loc.split(',')[1];
+        const Longitude = locationData.loc.split(',')[0];
+        numbers.push(Latitude, Longitude);
+        numbers.push(Latitude, Longitude);
+        resolve(numbers);
+        // console.log(numbers);
+      });
+
+      res.on('error', (error) => {
+        reject(error);
+      });
+    });
+  });
+}
+try {
+  const fetchedData = await fetchDataAndProcess();
+} catch (error) {
+  console.error('Error:', error);
+}
+//---------------------------------------------
+const API_URL = `https://api.openuv.io/api/v1/uv?lat=${numbers[3]}&lng=${numbers[2]}`;
+numbers.length = 0;
+// console.log(API_URL);
+// console.log(numbers);
   try {
     const result = await axios.get(API_URL, {
       headers:{
-        "x-access-token" : "openuv-km2rm0jn8q0x-io",
+        "x-access-token" : "openuv-km2rm0jeisla-io",
       }
     });
 
@@ -29,17 +72,18 @@ app.get("/", async (req, res) => {
       uv : "UV INDEX: " + result.data.result.uv,
       muv : "MAX UV TODAY: "+result.data.result.uv_max,
       a : av,
-      dt: date.toLocaleDateString() + " "+" "+ time,
+      dt: date.toLocaleDateString() + " "+" "+ time +" IST",
     });
   } 
   catch (error) {
     res.render("index.ejs", { 
-      uv : "4",
-      muv: "11",
-      a : av,
-      dt: "Daily API quota exceeded !!! TRY AGAIN LATER",
+      uv : "NA",
+      muv: "NA",
+      a : "NA",
+      dt: "API quota exceeded !!! TRY AGAIN LATER",
     });
   }
+
 });
 
 app.listen(port, () => {
