5acacdcde6b63a5672e2b0cd9b412538d98afc9d
mdrahilsubhan2 <mdrahilsubhan26@gmail.com>
2024-10-19 19:12:12 +0530

    OPTIMISED

--- index.js
+++ index.js
@@ -5,60 +5,53 @@ import https from "https";
 const app = express();
 const port = 3000;
 const numbers = new Array(2); 
+var Longitude=0;
+var Latitude=0;
+const options = {
+  hostname: 'ipinfo.io',
+  path: '/json',
+}; 
 
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-
-async function fetchDataAndProcess() {
-  const options = {
-    hostname: 'ipinfo.io',
-    path: '/json',
-  }; 
-
-  return new Promise((resolve, reject) => {
-    https.get(options, (res) => {
-      let dataChunks = [];
-
-      res.on('data', (chunk) => {
+  async function fetchDataAndProcess() {
+    return new Promise((resolve, reject) => {
+      https.get(options, (res) => {
+        let dataChunks = [];
+        res.on('data', (chunk) => {
         dataChunks.push(chunk);
-      });
+        });
 
-      res.on('end', () => {
-        const data = Buffer.concat(dataChunks).toString();
-        const locationData = JSON.parse(data);
-        const Latitude = locationData.loc.split(',')[1];
-        const Longitude = locationData.loc.split(',')[0];
-        numbers.push(Latitude, Longitude);
-        numbers.push(Latitude, Longitude);
-        resolve(numbers);
-        // console.log(numbers);
-      });
+        res.on('end', () => {
+          const data = Buffer.concat(dataChunks).toString();
+          const locationData = JSON.parse(data);
+          Latitude = locationData.loc.split(',')[1];
+          Longitude = locationData.loc.split(',')[0];   
+          resolve("TRUE");
+        });
 
-      res.on('error', (error) => {
-        reject(error);
+        res.on('error', (error) => {
+          reject(error);
+        });
+
       });
     });
-  });
-}
-try {
-  const fetchedData = await fetchDataAndProcess();
-} catch (error) {
-  console.error('Error:', error);
-}
-//---------------------------------------------
-const API_URL = `https://api.openuv.io/api/v1/uv?lat=${numbers[3]}&lng=${numbers[2]}`;
-numbers.length = 0;
-// console.log(API_URL);
-// console.log(numbers);
+  }
+
+  try {
+    await fetchDataAndProcess();
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
+  const API_URL = `https://api.openuv.io/api/v1/uv?lat=${Longitude}&lng=${Latitude}`;
   try {
     const result = await axios.get(API_URL, {
       headers:{
         "x-access-token" : "openuv-km2rm0jeisla-io",
       }
     });
-
-    
     const dateTime = result.data.result.uv_time;
     const date = new Date(dateTime);
     const time = date.toLocaleTimeString();
